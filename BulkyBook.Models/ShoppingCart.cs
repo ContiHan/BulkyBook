47bd6f80bec2339629c54e8f4586e591875c0ff3
contihan <daniel.hanak@outlook.cz>
2022-07-06 18:27:04 +0200

    add shopping cart to migration and database

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,9 +10,19 @@ namespace BulkyBook.Models
 {
     public class ShoppingCart
     {
-        public Product? Product { get; set; }
+        public int Id { get; set; }
 
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        public Product? Product { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
