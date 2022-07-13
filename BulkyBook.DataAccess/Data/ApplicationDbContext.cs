30125f05f8b321c4b3e5de6a7fe1acb72bd31fec
contihan <daniel.hanak@continental-corporation.com>
2022-07-13 07:06:21 +0200

    model for order header and order detail

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -19,5 +19,9 @@ namespace BulkyBook.Data
         public DbSet<Company>? Companies { get; set; }
 
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
